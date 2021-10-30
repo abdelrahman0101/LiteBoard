ca69f8f9964f33c142782e46c6c57b34b92f1001
Abdelrahman Abdelkader <abdelrahman.em@gmail.com>
2021-10-30 12:01:35 +0200

    New features and bug fixes

--- README.md
+++ README.md
@@ -4,29 +4,25 @@ A simple light-weight browser-based whiteboard application.
 ## Current Features:
 * A set of commonly needed drawing tools:
   1. Standard pen with a circular stroke.
-  2. Calligraphic pen with an elliptical stroke supporting dynamic line width based on pressure and pointer acceleration. Currently configured for use in writing Naskh and Tholoth with a rotation angle of 70 degrees.
+  2. Calligraphic pen with an elliptical stroke supporting dynamic line width based on pointer acceleration. Multiple neb angles are now supported.
   3. Highlighter pen for highlighting content with a transparent color.
   4. Eraser with a transparent background.
 
 * Support for multiple pages.
-* Import a single image into the active page.
-* Export the active page to an image in a new window.
-* Change the background color of the board anytime independently from its content.
-
-## Currently known bugs:
-1. Switching to another non-empty page while the eraser tool is active erases the destination page.
-2. Canvas doesn't fit the browser window in Chrome/Edge.
-3. Calligraphic pen is not working when Windows Ink is enabled with some graphics tablets e.g. Huion Kamvas 13.
+* Import multiple images or PDF files.
+* Paste images from clipboard into the active page.
+* Export and download the active page as an image.
+* Change the background color of the board anytime independently of its content.
 
 ## Planned Features:
-1. Custom cursors for different tools.
+1. Custom pointer cursors for different tools.
 2. ~~Interactive styles for the tool buttons to indicate the active tool.~~ **Done**
 3. ~~Use custom icons for tool buttons instead of (emoji) unicode characters.~~ **Done**
 4. Use a responsive design to support mobile devices.
 5. A new icon for the app and website.
 6. Select and move any part of the image.
-7. Change pen angle with the calligraphic pen.
-8. Import multiple images to multiple pages.
-9. Import multi-page PDF files based on PDF.js.
+7. ~~Change pen angle with the calligraphic pen.~~ **Done**
+8. ~~Import multiple images to multiple pages.~~ **Done**
+9. ~~Import multi-page PDF files based on PDF.js.~~ **Done**
 10. Custom color picker.
 11. Integration with cloud-based storage services e.g. Google Drive, to store and import files.
--- board.js
+++ board.js
@@ -2,7 +2,7 @@ var cnvs = document.querySelector("#board");
 var ctx = cnvs.getContext("2d");
 var lineWidth = 2;
 //ctx.translate(0.5, 0.5);
-
+const canvas_resolution_scale = 1.0;
 var drawing = false;
 var x = 0;
 var y = 0;
@@ -22,7 +22,8 @@ var tool_options = {
     "calligraphy": {
         lineWidth: 3,
         strokeStyle: '#000000',
-        alpha: 1
+        alpha: 1,
+        angle: Math.PI / 9
     },
     "highlighter" : {
         lineWidth: 5,
@@ -47,17 +48,16 @@ cnvs.addEventListener("pointerdown", function (event) {
     var x = event.clientX - cnvs.getBoundingClientRect().left;
     var y = event.clientY - cnvs.getBoundingClientRect().top;
     ctx.beginPath();
-    ctx.moveTo(x, y);
+    ctx.moveTo(x * canvas_resolution_scale, y * canvas_resolution_scale);
     current_path = [];
     current_path.push({x: x, y: y, lineWidth: lineWidth});
     ctx.lineWidth = lineWidth; // set line width according to the settings of the selected tool
 });
 
-
 cnvs.addEventListener('pointermove', function (event) {
-    var x = event.clientX - cnvs.getBoundingClientRect().left;
-    var y = event.clientY - cnvs.getBoundingClientRect().top;
-    showStatus("X: " + Math.round(x) + ", Y: " + Math.round(y));
+    var x = (event.clientX - cnvs.getBoundingClientRect().left) * canvas_resolution_scale;
+    var y = (event.clientY - cnvs.getBoundingClientRect().top) * canvas_resolution_scale;
+    //showStatus("X: " + Math.round(x) + ", Y: " + Math.round(y));
     if (drawing) {
         //console.log("Pen pressure: " + event.pressure);
         //const pressure = (event.pressure + 0.5);
@@ -75,7 +75,7 @@ cnvs.addEventListener('pointermove', function (event) {
 
             ctx.lineWidth = newWidth;
             ctx.lineTo(x, y);
-            ctx.rotate(Math.PI / 9);
+            ctx.rotate(tool_options.calligraphy.angle);
             ctx.scale(0.1, 1);
             ctx.stroke();
             ctx.resetTransform();
@@ -99,14 +99,16 @@ cnvs.addEventListener('pointermove', function (event) {
 
 function strokeCalligraphyPoint(x, y){
     ctx.lineTo(x, y);
-    ctx.rotate(Math.PI / 9);
+    ctx.rotate(tool_options.calligraphy.angle);
     ctx.scale(0.1, 1);
     ctx.stroke();
     ctx.resetTransform();
 }
 
+
 cnvs.addEventListener("pointerup", function () {
     drawing = false;
+    saveCurrentPage();
 });
 
 cnvs.addEventListener("pointerout", function () {
@@ -121,7 +123,7 @@ function showStatus(txt) {
 document.querySelectorAll(".color").forEach(c => {
     c.addEventListener("click", function (event) {
         var color = window.getComputedStyle(event.target).backgroundColor;
-        if (selected_tool == "eraser") {
+        if (selected_tool === "eraser") {
             selected_tool = "pen";
             load_options("pen");
         }
@@ -163,7 +165,10 @@ document.querySelector('#btnNew').addEventListener("click", function () {
 
 document.querySelector("#btnSave").addEventListener("click", function () {
     saveCurrentPage();
-    window.open(cnvs.toDataURL("image/png"));
+    let downloadLink = document.createElement("a");
+    downloadLink.setAttribute("download", "Page" + (pageIndex + 1) + ".png");
+    downloadLink.href = cnvs.toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+    downloadLink.click();
 });
 
 document.querySelector("#btnNext").addEventListener("click", function () {
@@ -183,6 +188,7 @@ document.querySelector("#btnEraser").addEventListener("click", function () {
 document.querySelector("#btnClear").addEventListener("click", function () {
     // TODO: need confirmation first
     clearBoard();
+    saveCurrentPage();
 });
 
 document.querySelector("#btnPen").addEventListener("click", function () {
@@ -191,6 +197,36 @@ document.querySelector("#btnPen").addEventListener("click", function () {
 
 document.querySelector("#btnCalligraphy").addEventListener("click", function () {
     load_options("calligraphy");
+});
+
+document.querySelectorAll("#calligraphy_angles button").forEach((item)=>{
+    item.addEventListener("click", function (e){
+        document.querySelector("#calligraphy_angles button.enabled").classList.remove("enabled");
+        e.target.classList.add("enabled");
+        const angle = Number(e.target.value);
+        tool_options.calligraphy.angle = (90 - angle) * Math.PI / 180;
+        hideDropdownLists();
+    });
+});
+
+document.querySelectorAll(".dropdown_container > button").forEach((btn)=>{
+    btn.addEventListener("click", (e)=> {
+        document.querySelector("#float_screen").style.visibility = "visible";
+        e.target.nextElementSibling.style.visibility = "visible";
+    });
+});
+
+function hideDropdownLists()
+{
+    document.querySelectorAll(".dropdown_list").forEach((lst)=>{
+        lst.style.visibility = "hidden";
+        document.querySelector("#float_screen").style.visibility = "hidden";
+    });
+}
+
+//hide dropdown lists when they lose focus
+document.querySelector("#float_screen").addEventListener("click", function (){
+    hideDropdownLists();
 });
 
 document.querySelector("#btnHighlight").addEventListener("click", function () {
@@ -202,28 +238,149 @@ document.querySelector("#btnImport").addEventListener("click", function () {
 });
 
 document.querySelector("#file_importer").addEventListener("change", function (event) {
-    var img = new Image;
+    const file_urls = event.target.files;
+    for (let i=0; i < file_urls.length; i++)
+    {
+        const ext = file_urls[i].name.slice(-4).toLowerCase();
+        if (ext === '.pdf')
+        {
+            const fileReader = new FileReader();
+            fileReader.onload = function() {
+                const typedarray = new Uint8Array(this.result);
+                loadPDF(typedarray);
+            };
+            fileReader.readAsArrayBuffer(file_urls[i]);
+        }
+        else
+        {
+            let img = new Image;
+            img.onload = function () {
+                insertPageFromImage(img, pageIndex + 1);
+                img.onload = null;
+            };
+            img.src = URL.createObjectURL(file_urls[i]);
+        }
+    }
+});
+
+function loadPDF(pdfData)
+{
+    // Loaded via <script> tag, create shortcut to access PDF.js exports.
+    const pdfjsLib = window['pdfjs-dist/build/pdf'];
+    // The workerSrc property shall be specified.
+    pdfjsLib.GlobalWorkerOptions.workerSrc = './pdfjs-2.10.377-dist/build/pdf.worker.js';
+    const loadingTask = pdfjsLib.getDocument(pdfData);
+    loadingTask.promise.then(function(pdf) {
+        console.log('PDF loaded');
+        const page_count = pdf.numPages;
+        console.log("Number of pages:", page_count);
+        /*
+            Pages are rendered asynchronously.
+            We need to insert PDF pages in order, but some pages take more time to render than others
+         */
+        let temp_cnvs_array = new Array(page_count);
+        let progress = 0;
+        const progress_bar = document.querySelector("#fileProgress");
+        progress_bar.value = 0;
+        progress_bar.style.visibility = "visible"
+        for (let i=1; i<=page_count;i++)
+        {
+            const temp_cnvs = document.createElement('canvas');
+            const context = temp_cnvs.getContext('2d');
+            (function (pdf_idx){
+                //create a new scope to preserve PDF page index
+                pdf.getPage(i).then(function(page) {
+                    console.log('Page loaded', i);
+                    let scale_to_fit = 1;
+                    const pageWidth = page.view[2];
+                    const pageHeight = page.view[3];
+                    if (pageHeight > pageWidth)
+                    {
+                        scale_to_fit = cnvs.parentElement.clientWidth / pageWidth;
+                    }
+                    else
+                    {
+                        scale_to_fit = cnvs.parentElement.clientHeight / pageHeight;
+                    }
+                    const viewport = page.getViewport({scale: scale_to_fit * canvas_resolution_scale});
+
+                    // Prepare canvas using PDF page dimensions
+                    temp_cnvs.height = viewport.height;
+                    temp_cnvs.width = viewport.width;
+                    // Render PDF page into canvas context
+                    const renderContext = {
+                        canvasContext: context,
+                        viewport: viewport
+                    };
+                    const renderTask = page.render(renderContext);
+                    renderTask.promise.then(function () {
+                        temp_cnvs_array[i - 1] = temp_cnvs;
+                        progress++;
+                        progress_bar.value = progress * 100 / temp_cnvs_array.length;
+                        if (progress === temp_cnvs_array.length)
+                        {
+                            progress_bar.style.visibility = "hidden";
+                            //all pages have been loaded
+                            for (let c=0; c < temp_cnvs_array.length; c++)
+                            {
+                                const img = new Image();
+                                img.src = temp_cnvs_array[c].toDataURL();
+                                //create a new Opelea page from rendered pdf page
+                                insertPageFromImage(img, pageIndex + c + 1);
+                            }
+                        }
+                        else {
+                            //multiple files could be loading
+                            //TODO: Should we have multiple progress bars?
+                            progress_bar.style.visibility = "visible";
+                        }
+                    });
+                });
+            })(i);
+        }
+    }, function (reason) {
+        // PDF loading error
+        console.error(reason);
+    });
+}
+
+function pasteFromClipboard(e){
+    if (!e.clipboardData || !e.clipboardData.items){
+        return;
+    }
+    let items = e.clipboardData.items;
+    if (items[0].type.indexOf("image") === -1)
+        return;
+    let blob = items[0].getAsFile();
+    let img = new Image;
     img.onload = function () {
         ctx.drawImage(img, 0, 0);
         URL.revokeObjectURL(img.src);
+        saveCurrentPage();
     };
-    img.src = URL.createObjectURL(event.target.files[0]);
+    img.src = URL.createObjectURL(blob);
+}
+
+//document.querySelector("#btnPaste").addEventListener("click", pasteFromClipboard);
+window.addEventListener("paste", pasteFromClipboard);
+
+//show active tool style
+document.querySelectorAll(".tool").forEach((t)=>{
+    t.addEventListener("click", function (e){
+        const current = document.querySelector(".tool.enabled");
+            if (current)
+                current.classList.remove("enabled");
+        e.target.classList.add("enabled");
+    });
 });
 
-function newPage() {
-    if (pages.length > 0) {
-        saveCurrentPage();
-        clearBoard();
-    }
-    var img = new Image();
-    img.src = cnvs.toDataURL();
+function insertPageFromImage(img, pg_index){
     img.className = "thumb_img";
-    pageIndex++;
-    pages.splice(pageIndex, 0, img);
-    if (pageIndex > 0) {
-        pages[pageIndex - 1].after(img)
-        // fix indecies in event handlers
-        for (var i = pageIndex; i < pages.length; i++) {
+    pages.splice(pg_index, 0, img);
+    if (pg_index > 0) {
+        pages[pg_index - 1].after(img)
+        // fix indices in event handlers
+        for (var i = pg_index; i < pages.length; i++) {
             (function (idx) {
                 pages[i].onclick = function () {
                     navigateTo(idx)
@@ -239,8 +396,19 @@ function newPage() {
             img.onclick = function () {
                 navigateTo(idx)
             }
-        })(pageIndex);
+        })(pg_index);
     }
+}
+
+function newPage() {
+    if (pages.length > 0) {
+        saveCurrentPage();
+        clearBoard();
+    }
+    pageIndex++;
+    const img = new Image();
+    img.src = cnvs.toDataURL();
+    insertPageFromImage(img, pageIndex);
     setActivePage(pageIndex);
 }
 
@@ -251,18 +419,28 @@ function saveCurrentPage() {
 }
 
 function clearBoard() {
-    ctx.clearRect(0, 0, cnvs.clientWidth, cnvs.clientHeight);
+    ctx.clearRect(0, 0, cnvs.clientWidth * canvas_resolution_scale, cnvs.clientHeight * canvas_resolution_scale);
+
 }
 
 function navigateTo(target_page) {
-    if (target_page == pageIndex) {
+    if (target_page === pageIndex) {
         return;
     }
     saveCurrentPage();
     clearBoard();
     setActivePage(target_page);
-    
+    ctx.globalCompositeOperation = "source-over"; //needed when the eraser is active
+    //load selected page onto the canvas
+    cnvs.width = pages[target_page].naturalWidth;
+    cnvs.height = pages[target_page].naturalHeight;
+    cnvs.style.width = cnvs.width / canvas_resolution_scale + "px";
+    cnvs.style.height = cnvs.height / canvas_resolution_scale + "px";
+    // reset the lineCap and lineJoin properties to "round" after resizing the canvas
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
     ctx.drawImage(pages[target_page], 0, 0);
+    load_options(selected_tool);
     pageIndex = target_page;
 }
 
@@ -274,7 +452,7 @@ function setActivePage(index) {
 }
 
 function load_options(tool) {
-    if (tool == "eraser") {
+    if (tool === "eraser") {
         ctx.globalCompositeOperation = "destination-out"
     } else {
         ctx.globalCompositeOperation = "source-over"
@@ -291,8 +469,9 @@ function load_options(tool) {
 const css_w = window.getComputedStyle(cnvs).width;
 const css_h = window.getComputedStyle(cnvs).height;
 // fit drawing context size to canvas size
-cnvs.width = css_w.substring(0, css_w.length - 2);
-cnvs.height = css_h.substring(0, css_h.length - 2);
+cnvs.width = css_w.substring(0, css_w.length - 2) * canvas_resolution_scale;
+cnvs.height = css_h.substring(0, css_h.length - 2) * canvas_resolution_scale;
+console.log(cnvs.width, cnvs.height);
 // fix canvas size
 cnvs.style.width = css_w;
 cnvs.style.height = css_h;
